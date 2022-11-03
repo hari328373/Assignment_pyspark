d3380fb8cbf5fcb2cc729049aec9b0f2621f913e
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-11-03 16:33:42 +0530

    utility code for Pyspark Assignment

--- Pyspark/sample/_utilis_code.py
+++ Pyspark/sample/_utilis_code.py
@@ -2,8 +2,6 @@
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from datetime import datetime,date
 
 # create the session
 def Session():
