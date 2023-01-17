2d1adbf28bb33d2e0386f468dc7c6aa593e9afc0
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2023-01-17 21:42:34 +0530

    Add files via upload

---
+++ Spark_2/core/sample_code.py
@@ -0,0 +1,135 @@
+# Databricks notebook source
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import split, col, count, hour, minute, second
+
+
+def createSession():
+    s=SparkSession.builder.appName("sparkbyexamples").getOrCreate()
+    return s
+spark=createSession()
+
+
+def create_Torrent_df(spark,col1,rename_col_1,col2,rename_col_2,col3,rename_col_3, path):
+    df = spark.read.csv(path)
+    ghtorrent_df = df.withColumnRenamed(col1,rename_col_1).withColumnRenamed(col2,rename_col_2).withColumnRenamed(col3,rename_col_3)
+    return ghtorrent_df
+df1 = create_Torrent_df(spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2",
+                              "ghtorrent_details","dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+
+
+# converting structure of logfile 
+
+def req_Col(ghtorrent_log_df):
+    api_cliendId= ghtorrent_log_df .withColumn("ghtorrent_client_id", split(col("ghtorrent_details"), "--").getItem(0)) \
+        .withColumn("repository", split(col("ghtorrent_details"), "--").getItem(1)) \
+        .withColumn("downloader_id", split(col("ghtorrent_client_id"), "-").getItem(1)) \
+        .withColumn("repository_torrent", split(col("repository"), ':').getItem(0)) \
+        .withColumn("Request_status_ext", split(col("repository"), ':').getItem(1)) \
+        .withColumn("Request_status", split(col("Request_status_ext"), ",").getItem(0)) \
+        .drop(col("repository")) \
+        .withColumn("request_url", split(col("ghtorrent_details"), "URL:").getItem(1)) \
+        .drop(col("ghtorrent_details"))
+    return api_cliendId
+
+ghtorrent_df = req_Col(df1)
+print("Printing the torrent df")
+ghtorrent_df.show(truncate=False)
+
+
+# COMMAND ----------
+
+
+#  How many lines does gh_torrent contain
+def count_lines(df1):
+    count_line = df1.agg(count("*").alias("total_count_lines"))
+    count_line.sort("total_count_lines")
+    return count_line
+
+total_count = count_lines(ghtorrent_df)
+print("Total number of lines")
+total_count.show()
+
+
+# COMMAND ----------
+
+# count the number of WARNing messages
+def WARN_count(df1):
+    wranlog = df1.filter("LogLevel = 'WARN'").agg(count("*").alias("totalWARN_count"))
+    wranlog.sort("totalWARN_count")
+    return wranlog
+num_WARN = WARN_count(df1)
+print("number of WARNing")
+num_WARN.show()
+
+
+# COMMAND ----------
+
+# How many repositories where processed in total? Use the api_client lines only
+def api_client(df1):
+    rep_api = df1.filter(col("ghtorrent_details").like("%api_client.rb%")).agg(count("*").alias("api_claim"))
+    rep_api.sort("api_claim")
+    return rep_api
+api= api_client(df1)
+print("api_cilent repositories")
+api.show()
+
+
+# COMMAND ----------
+
+# which client did most HTTP requests?
+def http_req(df1):
+    http_req = df1.groupBy("ghtorrent_details").agg(count("*").alias("http_req_count"))
+    http_req.sort("http_req_count")
+    return http_req
+http = http_req(df1)
+print("active http requests")
+http.show(truncate=False)
+
+# COMMAND ----------
+
+# Which client did most FAILED HTTP requests? Use group by to provide an answer
+def fail_http(df1):
+    Failed_req = df1.filter(col("ghtorrent_details").like("%Failed%")).groupBy("ghtorrent_details").agg(count("*").alias("Failed_request"))
+    Failed_req.sort("Failed_request")
+    return Failed_req
+fail_https=fail_http(df1)
+print("FAILED HTTP requests")
+fail_https.show(truncate=False)
+
+
+# COMMAND ----------
+
+# What is the most active hour of day
+def active_hr(df1):
+    active_hr = df1.withColumn("hour", hour(col("timestamp"))).groupBy("hour").agg(count("*").alias("active_hours"))
+    active_hr.sort("active_hours")
+    return active_hr
+ac_hr = active_hr(df1)
+print("most active hour of day")
+ac_hr.show(truncate=False)
+
+
+
+# COMMAND ----------
+
+# count of Active repos
+def active_repos(df1):
+    active_repository = df1.groupBy("ghtorrent_details").agg(count("*").alias("active_repository"))
+    active_repository.sort("active_repository")
+    return active_repository
+ac_re = active_repos(df1)
+print("count of Active repos")
+ac_hr.show(truncate=False)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
---
+++ Spark_2/test/test_code.py
@@ -0,0 +1,101 @@
+# Databricks notebook source
+# MAGIC %run ../core/sample_code
+
+# COMMAND ----------
+
+# unittesting for Saprk_2 Assignment
+
+import unittest
+
+class UtilityTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        spark = createSparkSession()
+        cls.spark = spark
+        
+    def test_create_torrent(self):
+        ghtorrent_df = create_Torrent_df(self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details","dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+        count = torrent_df.count()
+        self.assertGreatEqual(count, 1)
+
+# COMMAND ----------
+
+ def test_req_Col(self):
+        ghtorrent_df = create_Torrent_df(self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details",
+                                     "dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+        ghtorrent_df_extract = req_Col(ghtorrent_df)
+        actual_output = ghtorrent_df_extract.columns
+        expected_output = ['LogLevel', 'timestamp', 'ghtorrent_client_id', 'downloader_id', 'repository_torrent', 'Request_status_ext', 'Request_status', 'request_url']
+        self.assertEqual(actual_output,expected_output)
+
+# COMMAND ----------
+
+ def test_count_lines(self):
+        ghtorrent_df = (self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details",
+                                     "dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+        no_of_lines = ghtorrent_df.count()
+        self.assertEqual(no_of_lines, 54)
+
+# COMMAND ----------
+
+def test_WARN_count(self):
+    ghtorrent_df = create_Torrent_df(self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details",
+                                     "dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+    expected_count =  WARN_count(torrent_df, "LogLevel", "WARN")["warn_count"]
+    self.assertEqual(expected_count, 3811)
+
+# COMMAND ----------
+
+def test_ApiClient(self):
+    ghtorrent_df = create_Torrent_df(self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details",
+                                     "dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+    torrent_df_extract = req_Col(ghtorrent_df)
+    expected_count = rep_api(torrent_df_extract, "api_client.rb", "repository_torrent")["api_client_repo_count"]
+    self.assertEqual(expected_count,37595)
+
+# COMMAND ----------
+
+def ClientReq_http(self):
+    ghtorrent_df = create_Torrent_df(self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details",
+                                     "dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+    torrent_df_extract = split_req_Col(torrent_df)
+    expected_ouput = ClientReq_http(torrent_df_extract, "request_url", "ghtorrent_client_id").first()["ghtorrent_client_id"]
+    actual_output = " ghtorrent-5 "
+    self.assertEqual(expected_ouput,actual_output)
+
+# COMMAND ----------
+
+def testGetFailed_fail_http(self):
+    torrent_df = create_Torrent_df(self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details",
+                                     "dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+    torrent_df_extract = split_req_col(torrent_df)
+    expected_output = testGetFailed_fail_http(torrent_df_extract, "Request_status_ext", "%Failed%", "ghtorrent_client_id",   "max_failed_req_client")["max_failed_req_client"]
+        actual_output = " ghtorrent-13 "
+        self.assertEqual(expected_output,actual_output)
+
+# COMMAND ----------
+
+ def test_A_hrs(self):
+        torrent_df =  create_Torrent_df(self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details",
+                                     "dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+        torrent_df_extract = split_req_col(torrent_df)
+        expected_output = test_A_hrs(torrent_df_extract).first()["active_hours"]
+        actual_output = 16
+        self.assertEqual(expected_output,actual_output)
+
+# COMMAND ----------
+
+ def test_active_repos(self):
+        torrent_df = create_Torrent_df(self.spark, "_c0", "LogLevel", "_c1", "timestamp", "_c2", "ghtorrent_details",
+                                    "dbfs:/FileStore/shared_uploads/harisumanjali328@outlook.com/ghtorrent_logs.txt")
+        torrent_df_extract = split_req_col(torrent_df)
+        expected_output = countrepo(torrent_df_extract, "repository_torrent", "active_repo_used").first()["active_repo_used"]
+        actual_output = "ghtorrent.rb"
+        self.assertEqual(expected_output,actual_output)
+        
+        @classmethod
+        def tearDownClass(cls):
+            cls.spark.stop()
+
+# COMMAND ----------
+
